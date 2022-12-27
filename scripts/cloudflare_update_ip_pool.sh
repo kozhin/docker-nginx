b02e0d9dde696b87df19a0d41241e75dcc8f2a41
Konstantin Kozhin <1387510+kozhin@users.noreply.github.com>
2022-12-27 18:36:29 +0300

    CloudFlare update script changed

--- scripts/cloudflare_update_ip_pool.sh
+++ scripts/cloudflare_update_ip_pool.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 
 CF_CONFIG='/etc/nginx/conf.d/cloudflare.conf'
+TIMESTAMP=$(date)
 
-function fetch_cloudflare_list() {
-  LIST=`curl -s https://www.cloudflare.com/ips-${1}`
-  IPS=`echo "$LIST" | awk '{ print $1 }'`
-  for IP in ${IPS[@]}
-  do
-    echo "set_real_ip_from ${IP};" >> $CF_CONFIG
-  done
-}
+echo -e "\n# Automatically updated on ${TIMESTAMP}" > $CF_CONFIG
 
-TIMESTAMP=`date`
-echo "\n# Automatically updated at ${TIMESTAMP}" > $CF_CONFIG
+URL='https://www.cloudflare.com/ips-v4'
 
-echo "\n# https://www.cloudflare.com/ips-v4\n" >> $CF_CONFIG
-fetch_cloudflare_list 'v4'
+echo -e "\n# ${URL}\n" >> $CF_CONFIG
+LIST=$(curl -s ${URL})
+IPS=$(echo "$LIST" | awk "{ print $1 }")
+IPS=$(echo "${IPS}" | tr "\n" "|")
+while [ -n "$IPS" ]; do IP=${IPS%%|*}; echo "set_real_ip_from ${IP};" >> $CF_CONFIG; IPS=${IPS#*|}; done
+unset IP IPS LIST URL
 
-echo "\n# https://www.cloudflare.com/ips-v6\n" >> $CF_CONFIG
-fetch_cloudflare_list 'v6'
+URL='https://www.cloudflare.com/ips-v6'
 
-echo "\n# Enable real IP from IPs above\n" >> $CF_CONFIG
+echo -e "\n# ${URL}\n" >> $CF_CONFIG
+LIST=$(curl -s ${URL})
+IPS=$(echo "$LIST" | awk "{ print $1 }")
+IPS=$(echo "${IPS}" | tr "\n" "|")
+while [ -n "$IPS" ]; do IP=${IPS%%|*}; echo "set_real_ip_from ${IP};" >> $CF_CONFIG; IPS=${IPS#*|}; done
+unset IP IPS LIST URL
+
+echo -e "\n# Enable real IP from IPs above\n" >> $CF_CONFIG
 echo "real_ip_header CF-Connecting-IP;" >> $CF_CONFIG
 echo "# real_ip_header X-Forwarded-For; # this is optional and must be used separately from directive above" >> $CF_CONFIG
 
